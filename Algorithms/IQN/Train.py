4dea294734dd8c9ed12e973067e4ae74f3928799
Matthew Leigh <mattcleigh@gmail.com>
2020-07-15 20:27:32 +0200

    New PC test

--- Algorithms/IQN/Train.py
+++ Algorithms/IQN/Train.py
@@ -41,23 +41,23 @@ def main():
                     name    = env_name + "_" + alg_name,
                     net_dir = home_env + "Saved_Models/" + alg_name,
                     \
-                    gamma = 0.99, lr = 1e-3, grad_clip = 10,
+                    gamma = 0.99, lr = 1e-3, grad_clip = 0,
                     \
                     input_dims = inp_space, n_actions = act_space,
-                    depth = 2, width = 512, activ = nn.ReLU(),
+                    depth = 2, width = 256, activ = nn.ReLU(),
                     noisy = True, duel = True,
                     \
                     eps = 1.0, eps_min = 0.01, eps_dec = 5e-5,
                     \
                     mem_size    = 100000, batch_size = 64,
-                    target_sync = 5e-3, freeze_up = 10000,
+                    target_sync = 1e-2, freeze_up = 10000,
                     \
                     PER_on    = True, n_step   = 3,
                     PEReps    = 0.01, PERa     = 0.5,
                     PERbeta   = 0.4,  PERb_inc = 1e-6,
                     PERmax    = 1,
                     \
-                    n_quantiles = 32
+                    n_quantiles = 16
                     )
 
     if load_prev:
